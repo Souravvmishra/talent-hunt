b6d5471f84a9a108221e8877e59b9a4f2dace420
SouravvMishra <souravvmishra@gmail.com>
2023-01-08 17:01:20 +0530

    Added Some Animations

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ const show = () => {
     main.classList.remove("hidden")
     main.classList.add("flex")
     btn.classList.add("hidden")
+    $("footer").toggleClass("hidden");
     getQuiz()
-
+    $(".lightthisup").addClass("blazingStar");
+   
 }
 btn.addEventListener("click", show)
 
@@ -19,9 +21,11 @@ btn.addEventListener("click", show)
 
 async function getQuiz() {
     $(".result").hide();
+    $("#main").removeClass("blur-out-expand");
+
     for (let k = 0; k < optionDiv.length; k++) {
         optionDiv[k].innerHTML = `<button
-        class="option py-2 w-44 bg-transparent text-red-600 font-semibold border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 m-2 px-8 overflow-auto" >
+        class="option py-2 bg-transparent text-red-600 font-semibold border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 m-2 px-8 overflow-auto" >
         Loading...
     </button>`        
     }
@@ -36,7 +40,7 @@ async function getQuiz() {
     for (const key in data[0].answers) {
         if (data[0].answers[key] !== null) {
 
-            
+
             if (data[0].correct_answers[`${key}_correct`] === 'true') {
                 option[i].classList.add("correct")
             } else {
@@ -91,6 +95,8 @@ $(".option-div").on("click", ".option",  function () {
 $(".result").click(function (e) { 
     e.preventDefault();
     getQuiz()
+    $(".lightthisup").addClass("blur-out-expand");
+   
     
 });
 
